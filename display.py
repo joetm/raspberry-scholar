d13e6672ed7d2fabf13ba31d4f1e066ddf1467c5
Jonas Oppenlaender <oppenlaender@gmail.com>
2025-09-20 13:16:13 +0300

    update

--- display.py
+++ display.py
@@ -16,6 +16,7 @@ from PIL import Image, ImageDraw, ImageFont
 # ua = UserAgent(platforms='pc')
 # user_agent = ua.random
 user_agent = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:136.0) Gecko/20100101 Firefox/136.0"
+from scholarscrape import main as scrape
 
 
 # libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
@@ -25,6 +26,7 @@ BASEDIR = os.path.join(os.path.dirname(os.path.realpath(__file__)))
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--dev", dest="IS_DEV", action="store_true", help="Dev mode: output image")
+parser.add_argument("-s","--scholarid",dest="scholar_profile", help="Google Scholar ID")
 args = parser.parse_args()
 IS_DEV = args.IS_DEV
 print("DEV", IS_DEV)
@@ -66,7 +68,7 @@ class DISPLAY:
     else:
       self.image = Image.new('1', (self.epd.width, self.epd.height), 255)  # 255: clear the frame
 
-  def render(self, citations, hindex, diff, weekly_increase, monthly_increase):
+  def render(self, citations, hindex, diff, weekly_increase, monthly_increase, fiveyears):
     # image = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
     # draw = ImageDraw.Draw(image)
     # draw.rectangle([(2,2),(248,120)], outline=0)
@@ -81,6 +83,7 @@ class DISPLAY:
     draw = ImageDraw.Draw(self.image)
 
     draw.rectangle([(1,1),(249,121)], outline=0)
+    # draw.rectangle([(0,0),(250,122)], outline=0)
 
     draw.text((5, 0), str(citations), font=font_large, fill=0)
 
@@ -115,8 +118,6 @@ class DISPLAY:
     #   draw.text((165, 90), f"m{msign}{str(monthly_increase['citations_increase'])}", font=font_small, fill=0)
     # except: pass
 
-    print(fiveyears)
-
     barw, pad, H = 5, 5, 65
     minv, maxv = min(fiveyears), max(fiveyears)
     for i, y in enumerate(fiveyears):
@@ -138,32 +139,12 @@ class DISPLAY:
       self.image.save("output.png")
       self.image.show()
 
+
+
 # ---
-scholar_profile = 'ucO_QYQAAAAJ'
-scholar_url = f'https://scholar.google.com/citations?user={scholar_profile}&hl=en'
-result_file = os.path.join(BASEDIR, 'result.json')
 LOG_FILE = os.path.join(BASEDIR, "logs.json")
 # ---
 
-
-# Load logs
-try:
-    with open(LOG_FILE, "r") as f: logs = json.load(f)
-except FileNotFoundError:
-    logs = []
-
-# Clean old logs
-def clean_logs(logs):
-  one_month_ago = datetime.now() - timedelta(days=30)
-  logs = [log for log in logs if datetime.fromisoformat(log["timestamp"]) >= one_month_ago]
-  return logs
-
-# Add a new log entry
-def log_result(citations, hindex):
-  global logs
-  logs.append({"timestamp": datetime.now().isoformat(), "citations": citations, "hindex": hindex})
-  logs = clean_logs(logs)
-  with open(LOG_FILE, "w") as f: json.dump(logs, f)
 
 # Compute increases
 def compute_increase(latest, past_log):
@@ -175,66 +156,28 @@ def compute_increase(latest, past_log):
   return {"citations_increase": 0, "hindex_increase": 0}
 
 # Function to get the closest log entry before a given time
-def get_closest_log(past_time):
+def get_closest_log(past_time, logs):
   return max((log for log in logs if log["timestamp"] <= past_time), 
     key=lambda x: x["timestamp"], default=None)
 
-def get_earliest_log(past_time):
+def get_earliest_log(past_time, logs):
     eligible = [log for log in logs if log["timestamp"] <= past_time]
     if not eligible:
         return logs[0]  # fallback to oldest
     return max(eligible, key=lambda x: x["timestamp"])
 
-print(f"Scraping {scholar_url}...")
 
-cookie_file = os.path.join(BASEDIR, 'cookie.txt')
-with open(cookie_file, 'rt') as f: cookie = f.read()
-headers = {
-  'User-Agent': user_agent,
-  'referer': 'https://www.google.com/',
-  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-  'Accept-Encoding': 'gzip, deflate, br',
-  'Accept-Language': 'en-US,en;q=0.5',
-  'Cache-Control': 'no-cache',
-  'Connection': 'keep-alive',
-  'DNT': '1',
-  'Pragma': 'no-cache',
-  'Sec-Fetch-Dest': 'document',
-  'Sec-Fetch-Mode': 'navigate',
-  'Sec-Fetch-Site': 'none',
-  'Sec-Fetch-User': '?1',
-  'Sec-GPC': '1',
-  'Upgrade-Insecure-Requests': '1',
-  'Cookie': cookie.strip(),
-}
 
+res = scrape(["-s", args.scholar_profile])
+fiveyears = res['yearlycitations'][-5:]
+print("Last five years", fiveyears)
+
+
+# Load logs
 try:
-  response = requests.get(scholar_url, headers=headers)
-except requests.exceptions.ConnectTimeout as e:
-  print(f"ERROR ConnectTimeout: {requests.exceptions.ConnectTimeout}: {e}")
-  sys.exit()
-
-page_html = response.text
-soup = BeautifulSoup(page_html, 'html.parser')
-
-res = {'citations': None, 'hindex': None}
-stats = soup.select("table#gsc_rsb_st td.gsc_rsb_std") 
-if stats:
-  res['citations'] = int(stats[0].text)
-  res['hindex'] = int(stats[2].text)
-  log_result(res['citations'], res['hindex'])
-  print(f"Citations: {res['citations']}")
-  print(f"H-index: {res['hindex']}")
-
-fiveyears = []
-bars_box = soup.select("div.gsc_md_hist_b a.gsc_g_a span.gsc_g_al") 
-if bars_box:
-  for bar in bars_box:
-    count = int(bar.text)
-    fiveyears.append(count)
-fiveyears = fiveyears[-5:]
-# print("Last five years", fiveyears)
-
+    with open(LOG_FILE, "r") as f: logs = json.load(f)
+except FileNotFoundError:
+    logs = []
 
 # Convert timestamps to datetime objects and sort by time
 for log in logs: log["timestamp"] = datetime.fromisoformat(log["timestamp"])
@@ -251,14 +194,15 @@ if (latest is None) and (second is None):
   print("Not enough log entries. Exiting.")
   sys.exit()
 
+
 citations = str(latest["citations"])
 hindex = str(latest["hindex"])
 
 # time windows
 now = datetime.now()
-log_week = get_closest_log(now - timedelta(weeks=1))
-log_biweek = get_closest_log(now - timedelta(weeks=2))
-log_month = get_earliest_log(now - timedelta(days=30))
+log_week = get_closest_log(now - timedelta(weeks=1), logs)
+log_biweek = get_closest_log(now - timedelta(weeks=2), logs)
+log_month = get_earliest_log(now - timedelta(days=30), logs)
 
 weekly_increase = compute_increase(latest, log_week)
 biweekly_increase = compute_increase(latest, log_biweek)
@@ -282,7 +226,7 @@ else:
   # update the display
   # print("Update display...")
   epaper = DISPLAY(epd=epd, is_pi=IS_PI)
-  epaper.render(citations, hindex, diff, weekly_increase, monthly_increase)
+  epaper.render(citations, hindex, diff, weekly_increase, monthly_increase, fiveyears)
 
 
 # DEV = always pop up image on PC
@@ -290,5 +234,5 @@ if not IS_PI or IS_DEV:
   # output an image
   print("Output image...")
   epaper = DISPLAY(epd=epd, is_pi=IS_PI)
-  print('citations', citations, 'hindex', hindex, 'diff', diff, 'weekly_increase', weekly_increase, 'monthly_increase', monthly_increase)
-  epaper.render(citations, hindex, diff, weekly_increase, monthly_increase)
+  print('citations', citations, 'hindex', hindex, 'diff', diff, 'weekly_increase', weekly_increase, 'monthly_increase', monthly_increase, 'fiveyears', fiveyears)
+  epaper.render(citations, hindex, diff, weekly_increase, monthly_increase, fiveyears)
---
+++ scholarscrape.py
@@ -0,0 +1,176 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+
+import os
+import sys
+import json
+# import time
+# import logging
+import argparse
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
+from fake_useragent import UserAgent
+# ua = UserAgent(platforms='pc')
+# user_agent = ua.random
+user_agent = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:136.0) Gecko/20100101 Firefox/136.0"
+
+
+BASEDIR = os.path.join(os.path.dirname(os.path.realpath(__file__)))
+
+
+
+result_file = os.path.join(BASEDIR, 'citations.json')
+LOG_FILE = os.path.join(BASEDIR, "logs.json")
+# ---
+
+
+
+# Load logs
+try:
+  with open(LOG_FILE, "r") as f: logs = json.load(f)
+except FileNotFoundError:
+  logs = []
+
+# Clean old logs
+def clean_logs(logs):
+  one_month_ago = datetime.now() - timedelta(days=30)
+  logs = [log for log in logs if datetime.fromisoformat(log["timestamp"]) >= one_month_ago]
+  return logs
+
+# Add a new log entry
+def log_result(obj):
+  global logs
+  output = {"timestamp": datetime.now().isoformat()}
+  output.update(obj)
+  logs.append(output)
+  logs = clean_logs(logs)
+  with open(LOG_FILE, "w") as f: json.dump(logs, f)
+
+# Compute increases
+def compute_increase(latest, past_log):
+  if latest and past_log:
+    return {
+      "citations_increase": latest["citations"] - past_log["citations"],
+      "hindex_increase": latest["hindex"] - past_log["hindex"]
+    }
+  return {"citations_increase": 0, "hindex_increase": 0}
+
+# Function to get the closest log entry before a given time
+def get_closest_log(past_time):
+  return max((log for log in logs if log["timestamp"] <= past_time), 
+    key=lambda x: x["timestamp"], default=None)
+
+def get_earliest_log(past_time):
+    eligible = [log for log in logs if log["timestamp"] <= past_time]
+    if not eligible:
+        return logs[0]  # fallback to oldest
+    return max(eligible, key=lambda x: x["timestamp"])
+
+
+
+def build_parser():
+    p = argparse.ArgumentParser()
+    p.add_argument("-s", "--scholarid", dest="scholar_profile")
+    return p
+
+def main(argv=None):
+    args = build_parser().parse_args(argv)
+    return run(args.scholar_profile)
+
+def run(scholar_profile):
+  if not scholar_profile:
+    scholar_profile = 'ucO_QYQAAAAJ'
+  scholar_url = f'https://scholar.google.com/citations?user={scholar_profile}&hl=en'
+
+  print(f"Scraping {scholar_url}...")
+
+  cookie_file = os.path.join(BASEDIR, 'cookie.txt')
+  with open(cookie_file, 'rt') as f: cookie = f.read()
+  headers = {
+    'User-Agent': user_agent,
+    'referer': 'https://www.google.com/',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive',
+    'DNT': '1',
+    'Pragma': 'no-cache',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'none',
+    'Sec-Fetch-User': '?1',
+    'Sec-GPC': '1',
+    'Upgrade-Insecure-Requests': '1',
+    'Cookie': cookie.strip(),
+  }
+
+  try:
+    response = requests.get(scholar_url, headers=headers)
+  except requests.exceptions.ConnectTimeout as e:
+    print(f"ERROR ConnectTimeout: {requests.exceptions.ConnectTimeout}: {e}")
+    sys.exit()
+
+  page_html = response.text
+  soup = BeautifulSoup(page_html, 'html.parser')
+
+  # get total citations and hindex
+  res = {'citations': None, 'hindex': None}
+  stats = soup.select("table#gsc_rsb_st td.gsc_rsb_std") 
+  if stats:
+    res['citations'] = int(stats[0].text)
+    res['hindex'] = int(stats[2].text)
+    print(f"Citations: {res['citations']}")
+    print(f"H-index: {res['hindex']}")
+
+  # get citation history
+  yearlycitations = []
+  bars_box = soup.select("div.gsc_md_hist_b a.gsc_g_a span.gsc_g_al") 
+  if bars_box:
+    for bar in bars_box:
+      count = int(bar.text)
+      yearlycitations.append(count)
+  print("Citations per year", yearlycitations)
+
+
+  res['yearlycitations'] = yearlycitations
+
+  log_result(res)
+
+  return res
+
+
+  # # Convert timestamps to datetime objects and sort by time
+  # for log in logs: log["timestamp"] = datetime.fromisoformat(log["timestamp"])
+  # logs.sort(key=lambda x: x["timestamp"])
+
+  # # Get latest values
+  # try: latest = logs[-1]
+  # except: latest = None
+  # try: second = logs[-2]
+  # except: second = None
+
+  # # check if changes
+  # if (latest is None) and (second is None):
+  #   print("Not enough log entries. Exiting.")
+  #   sys.exit()
+
+  # citations = str(latest["citations"])
+  # hindex = str(latest["hindex"])
+
+  # # time windows
+  # now = datetime.now()
+  # log_week = get_closest_log(now - timedelta(weeks=1))
+  # log_biweek = get_closest_log(now - timedelta(weeks=2))
+  # log_month = get_earliest_log(now - timedelta(days=30))
+
+  # weekly_increase = compute_increase(latest, log_week)
+  # biweekly_increase = compute_increase(latest, log_biweek)
+  # monthly_increase = compute_increase(latest, log_month)
+
+
+if __name__ == "__main__":
+    main()
+
